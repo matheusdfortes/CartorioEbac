81bf559971013cc6276824dedadc421177234172
matheusdfortes <122515653+matheusdfortes@users.noreply.github.com>
2023-01-12 04:59:59 -0300

    Opção de inserir mais de um usuário no registro
    Incluso no sistema a opção de inserir mais um usuário após ter registrado algum antes

--- Cartório.c
+++ Cartório.c
@@ -10,73 +10,82 @@ int registro() //função de registro de novos usuários
 	char cpf[40];
 	char cargo[40];
 	char arquivo[40];
+	int resposta = 1;
 	
-	printf("Digite o cpf a ser cadastrado: "); //cadastrando a variável cpf
-	scanf("%s",cpf);
-	
-	strcpy(arquivo,cpf); //Copiando o valor da string
-	
-	FILE * file; // criando o arquivo
-	file = fopen(arquivo,"w");
-	fclose(file); // fechando o arquivo
-	
-	file = fopen(cpf,"a"); //atualizando o arquivo 
-	fprintf(file,"CPF: ");
-	fclose(file);
-	
-	file = fopen(cpf,"a"); //atualizando o arquivo com a varíavel cpf
-	fprintf(file,cpf);
-	fclose(file);
-	
-	file = fopen(arquivo,"a"); //atualizando o arquivo 
-	fprintf(file," \n");
-	fclose(file);
-	
-	printf("Digite o nome a ser cadastrado: "); //cadastrando a variável nome
-	scanf("%s",nome);
-	
-	file = fopen(cpf,"a"); //atualizando o arquivo
-	fprintf(file,"Nome: ");
-	fclose(file);
-	
-	file = fopen(arquivo,"a"); //atualizando o arquivo com a variável nome
-	fprintf(file,nome);
-	fclose(file);
-	
-	file = fopen(arquivo,"a"); //atualizando o arquivo com um virgula
-	fprintf(file," \n");
-	fclose(file);
-	
-	printf("Digite o sobrenome a ser cadastrado: "); //cadastrando a variável sobreNome
-	scanf("%s",sobreNome);
-	
-	file = fopen(cpf,"a"); //atualizando o arquivo
-	fprintf(file,"Sobrenome: ");
-	fclose(file);
-	
-	file = fopen(arquivo,"a"); //atualizando o arquivo com a variável sobreNome
-	fprintf(file,sobreNome);
-	fclose(file);
-	
-	file = fopen(arquivo,"a"); //atualizando o arquivo com um virgula
-	fprintf(file," \n");
-	fclose(file);
-	
-	printf("Digite o cargo: "); //cadastrando a variável cargo
-	scanf("%s",cargo);
-	
-	file = fopen(cpf,"a"); //atualizando o arquivo
-	fprintf(file,"Cargo: ");
-	fclose(file);
-	
-	file = fopen(arquivo,"a"); //atualizando o arquivo com a variável cargo
-	fprintf(file,cargo);
-	fclose(file);
-	
-	file = fopen(cpf,"a");
-	fprintf(file,"\n");
-	fclose(file);
-	
+	while(resposta == 1){		
+		printf("Digite o cpf a ser cadastrado: "); //cadastrando a variável cpf
+		scanf("%s",cpf);
+		
+		strcpy(arquivo,cpf); //Copiando o valor da string
+		
+		FILE * file; // criando o arquivo
+		file = fopen(arquivo,"w");
+		fclose(file); // fechando o arquivo
+		
+		file = fopen(cpf,"a"); //atualizando o arquivo 
+		fprintf(file,"CPF: ");
+		fclose(file);
+		
+		file = fopen(cpf,"a"); //atualizando o arquivo com a varíavel cpf
+		fprintf(file,cpf);
+		fclose(file);
+		
+		file = fopen(arquivo,"a"); //atualizando o arquivo 
+		fprintf(file," \n");
+		fclose(file);
+		
+		printf("Digite o nome a ser cadastrado: "); //cadastrando a variável nome
+		scanf("%s",nome);
+		
+		file = fopen(cpf,"a"); //atualizando o arquivo
+		fprintf(file,"Nome: ");
+		fclose(file);
+		
+		file = fopen(arquivo,"a"); //atualizando o arquivo com a variável nome
+		fprintf(file,nome);
+		fclose(file);
+		
+		file = fopen(arquivo,"a"); //atualizando o arquivo com um virgula
+		fprintf(file," \n");
+		fclose(file);
+		
+		printf("Digite o sobrenome a ser cadastrado: "); //cadastrando a variável sobreNome
+		scanf("%s",sobreNome);
+		
+		file = fopen(cpf,"a"); //atualizando o arquivo
+		fprintf(file,"Sobrenome: ");
+		fclose(file);
+		
+		file = fopen(arquivo,"a"); //atualizando o arquivo com a variável sobreNome
+		fprintf(file,sobreNome);
+		fclose(file);
+		
+		file = fopen(arquivo,"a"); //atualizando o arquivo com um virgula
+		fprintf(file," \n");
+		fclose(file);
+		
+		printf("Digite o cargo: "); //cadastrando a variável cargo
+		scanf("%s",cargo);
+		
+		file = fopen(cpf,"a"); //atualizando o arquivo
+		fprintf(file,"Cargo: ");
+		fclose(file);
+		
+		file = fopen(arquivo,"a"); //atualizando o arquivo com a variável cargo
+		fprintf(file,cargo);
+		fclose(file);
+		
+		file = fopen(cpf,"a");
+		fprintf(file,"\n");
+		fclose(file);
+		
+		printf("\nDeseja inserir mais algum usuário ?\n");
+		printf("1- Sim\n");
+		printf("2- Não\n");
+		scanf("%d",&resposta);
+		printf("\n");
+	} 
+		
 }
 
 int consultar() //função de consultar usuários
@@ -112,19 +121,19 @@ int deletar() //função de deletar usuários
 	char cpf[40]; //definindo variáveis
 	char decisao[1];
 	
-	printf("Digite o cpf do usuï¿½rio que vocï¿½ quer deletar: "); //definindo a variável cpf
+	printf("Digite o cpf do usuário que você quer deletar: "); //definindo a variável cpf
 	scanf("%s",cpf);
 	
 	FILE*file; //lendo o arquivo chamado cpf
 	file = fopen(cpf,"r");
 	
 	if(file == NULL){ //resposta caso o usuário não exista
-		printf("O Usuï¿½rio nï¿½o foi encontrado no sistema !\n "); 
+		printf("O usuário não foi encontrado no sistema !\n "); 
 		fclose(file);
 	}
 	
 	else{ //deletando o arquivo do usuário
-		printf("o usuï¿½rio foi deletado do sistema!\n");
+		printf("o usuário foi deletado do sistema!\n");
 		fclose(file);
 		remove(cpf);
 	}
